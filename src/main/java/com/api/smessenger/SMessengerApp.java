0a9d9f026704863e906caf818aeee0cc9f59988d
edwinans <edyansari5@gmail.com>
2025-10-09 14:24:09 +0200

    Init using spring initializr

---
+++ src/main/java/com/api/smessenger/SMessengerApp.java
@@ -0,0 +1,13 @@
+package com.api.smessenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMessengerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SMessengerApp.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/smessenger/SmessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.smessenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmessengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
